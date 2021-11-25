0beac7e17afd1ea4dc3342bad6f532bb29eb5706
navcsdev <nguyenanhvu.cs@gmail.com>
2021-11-26 00:20:15 +0700

    Improve migrate

--- migrations/3_deploy_musk_v2.js
+++ migrations/3_deploy_musk_v2.js
@@ -1,11 +1,12 @@
 const { deployProxy, upgradeProxy } = require('@openzeppelin/truffle-upgrades');
+const MetaMuskToken = artifacts.require("MetaMuskToken");
 const MetaMuskTokenV2 = artifacts.require("MetaMuskTokenV2");
 
 
 module.exports = async function (deployer, network) {
     console.log("you are deploying with the network: ", network);
 
-    const newInstance = await upgradeProxy('0x58ADdE577Fe71a4E1e4BA2e4AE5203b877C0F344', MetaMuskTokenV2, { deployer });
+    const newInstance = await upgradeProxy(MetaMuskToken.address, MetaMuskTokenV2, { deployer });
     console.table({
         MetaMuskTokenContractV2: newInstance.address
     });
