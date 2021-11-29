5a82e742329e6f8429abbe1712d7a6eae2664df6
khanh <khanhbvh93@gmail.com>
2021-11-29 12:21:49 +0700

    update

--- contracts/MetaMuskTokenV3.sol
+++ contracts/MetaMuskTokenV3.sol
@@ -648,10 +648,12 @@ contract MetaMuskTokenV3 is
     }
 
     function _buy(address sender, uint256 buyAmountToken) internal {
+        require(unlockTime != 0, "unlockTime must be != 0");
+
         if (users[sender].isSetup == false) {
             UserInfo storage userInfo = users[sender];
             userInfo.amountICO = buyAmountToken;
-            users[sender].claimAt = block.timestamp;
+            users[sender].claimAt = unlockTime;
             userInfo.amountClaimPerSec = _calTotalAmountPerSec(buyAmountToken);
             userInfo.isSetup = true;
         } else {
