b5feceba320a563bec945ec2a0ea4ce0101dc35f
khanh <khanhbvh93@gmail.com>
2021-11-29 17:43:49 +0700

    update

--- contracts/MetaMuskTokenV3.sol
+++ contracts/MetaMuskTokenV3.sol
@@ -218,19 +218,19 @@ contract MetaMuskTokenV3 is
     }
 
     function unlockToken() external {
+        address sender = _msgSender();
+        if (users[sender].claimAt < unlockTime)
+            users[sender].claimAt = unlockTime;
+
         require(
             unlockTime != 0 && unlockTime < block.timestamp,
             "some available balance has been locked and will be unlocked gradually after unlock time"
         );
 
-        address sender = _msgSender();
         require(
             users[sender].isSetup == true && users[sender].amountICO > 0,
             "no token locked to be unlocked"
         );
-
-        if (users[sender].claimAt < unlockTime)
-            users[sender].claimAt = unlockTime;
 
         uint256 unlockAmount = _getUnlockAmount(sender);
         if (unlockAmount > 0) {
